b797e07cd3e3976b2a314ffa5734ecbe60074d46
Melika Minaei <melika0422@gmail.com>
2024-07-10 11:42:38 -0400

    prompt fix

--- server.py
+++ server.py
@@ -29,11 +29,12 @@ class ImageServiceServicer(sendImage_pb2_grpc.ImageServiceServicer):
         pipe = pipe.to("cuda")
         
         prompt = request.name
+        prompt = "Texture of " + prompt + ", without visible edges, high resolution"
         print(f"PROMPT: {prompt}")
         print(len(prompt))
     
 
-        image_path = os.path.join(PATH, f"Tileable material of {prompt}.png")
+        image_path = os.path.join(PATH, f"{prompt}.png")
         print(f"IMAGE PATH: {image_path}")
 
         try:
